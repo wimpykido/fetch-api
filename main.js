de60b7a115b3c859e29b84d6cf793d012d38c3c5
wimpykido <eliso.saladze5@gmail.com>
2023-04-12 16:52:32 +0400

    1

---
+++ main.js
@@ -0,0 +1,63 @@
+const button = document.getElementById("btn")
+const output = document.querySelector(".output")
+const input = document.querySelector("#id");
+
+const getPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    data = await response.json();
+    return data;
+}
+const filterPosts = async () => {
+  const num = document.getElementById("id").value.trim();
+  try {       
+    const res = await getPosts();
+    if (num === "") {
+      console.log(res);
+      showOnScreen(res);
+    } else {
+      const filtered = res.filter(post => post.userId === parseInt(num));
+      showOnScreen(filtered);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+function showOnScreen (data) {
+  const output = document.querySelector(".output");
+
+  output.innerHTML = "";
+
+  data.forEach(({ title, userId, body }) => {
+    const container = document.createElement("div");
+
+    const head = document.createElement("h1");
+    head.textContent = title;
+
+    const userIdElement = document.createElement("p");
+    userIdElement.textContent = `User ID: ${userId}`;
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = body;
+
+    container.append(head, userIdElement, paragraph);
+    output.appendChild(container);
+  });
+  if (data.length === 0) {
+    const error = document.createElement("div");
+
+    error.className = "error";
+    error.textContent = "SORRY, Nothing Was Found";
+    
+    output.appendChild(error);
+  }
+}
+filterPosts();
+button.addEventListener("click", filterPosts);
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      button.click();
+  }
+});
+
+
